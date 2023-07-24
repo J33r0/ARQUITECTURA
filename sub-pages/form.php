650e0059c9a6e3dc85bc49719bf8e6951ed1ba00
J33r0 <121136323+J33r0@users.noreply.github.com>
2023-07-25 01:48:51 +0200

    Correct the height in all the pages

--- sub-pages/form.php
+++ sub-pages/form.php
@@ -5,19 +5,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    // Set the recipient email address
     $to = "arquitectura.strasbourg@gmail.com";
 
-    // Set the email subject
     $subject = "Projet pour $name";
 
-    // Compose the email body
     $body = "Nom: $name\n";
     $body .= "Telephone : $phone\n";
     $body .= "Email: $email\n";
     $body .= "Description du projet: $message\n";
 
-    // Send the email
     if (mail($to, $subject, $body)) {
         echo "success";
     } else {
